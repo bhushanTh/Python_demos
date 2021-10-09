e80076a9e6912a91a371f1d625ee56e2b550c510
bhushan <bhushan.thakare223@mail.com>
2021-10-10 01:11:34 +0530

    First commit

--- src/Webscrapping_weather_report.py
+++ src/Webscrapping_weather_report.py
@@ -13,16 +13,16 @@ page = requests.get('https://forecast.weather.gov/MapClick.php?lat=39.1184300000
 soup = BeautifulSoup(page.content,'html.parser')
 week = soup.find(id='seven-day-forecast-body')
 
-items = week.find_all(class_= 'forecast-tombstone')
+items = week.find_all(class_='forecast-tombstone')
 #print(items[0])
 
 items[0].find(class_='period-name').get_text()
 items[0].find(class_='short-desc').get_text()
-items[0].find(class_='temp').get_text()
+#items[0].find(class_='temp').get_text()
 
 period_names = [i.find(class_='period-name').get_text() for i in items ]
 short_desc = [i.find(class_='short-desc').get_text() for i in items ]
-tempr = [i.find(class_='temp').get_text() for i in items ]
+#tempr = [i.find(class_='temp').get_text() for i in items ]
 
 #print(period_names)
 #print(short_desc)
@@ -33,5 +33,6 @@ tempr = [i.find(class_='temp').get_text() for i in items ]
 weather_stuff = pd.DataFrame(
     {'period' : period_names,
      'short_Description': short_desc,
-     'Temperatures': tempr})
-weather_stuff.to_csv("C:\\Users\\Priyanka\\weather_report.csv")
+     #'Temperatures': tempr
+     })
+weather_stuff.to_csv("C:\\Users\\HP\\Desktop\\weather_report.csv")
